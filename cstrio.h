763297a7dbf759a795c0c389b2409ff3948afc80
Plato <otalpster@gmail.com>
2019-04-01 14:43:16 +0300

    cstrio.h: add function for freeing read file lines into words

--- cstrio.h
+++ cstrio.h
@@ -146,4 +146,16 @@ char ***fetch_words_lines(char *filename, size_t *n_lines, size_t **n_words) {
 	return lines;
 }
 
+void free_words_lines(char ***lines, size_t n_lines, size_t *n_words) {
+	for (size_t c_line = 0; c_line < n_lines; c_line++) {
+		for (size_t c_word = 0; c_word < n_words[c_line]; c_word++)
+			free(lines[c_line][c_word]);
+		free(lines[c_line]);
+	}
+	free(lines);
+	free(n_words);
+}
+
+// TODO: Implement write_words_lines() function which writes lines of words to a file.
+
 #endif  // #ifndef FOPS_H
